238b89a9ec79fe9ed7dc83a71a9eccc58c2f3cd5
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2021-09-30 23:18:05 +0530

    Authoriztion role based

--- src/main/java/com/example/springsec/Homeresource.java
+++ src/main/java/com/example/springsec/Homeresource.java
@@ -5,8 +5,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Homeresource {
+
     @GetMapping("/")
     public String home(){
         return ("Hello");
+    }
+    @GetMapping("/user")
+    public String user(){
+        return ("Hello, User");
+    }
+    @GetMapping("/admin")
+    public String admin(){
+        return ("Hello, Admin");
     }
 }
--- src/main/java/com/example/springsec/SecurityConfig.java
+++ src/main/java/com/example/springsec/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.springsec;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -11,12 +12,24 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("lakshan").password("lakshan")
-                .roles("USER");
+        auth.inMemoryAuthentication()
+                .withUser("lakshan").password("lakshan").roles("USER").and()
+                .withUser("admin").password("admin").roles("ADMIN").and()
+                .withUser("user").password("user").roles("USER");
     }
 
     @Bean
     public PasswordEncoder getPasswordEncoder(){
         return NoOpPasswordEncoder.getInstance();
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER","ADMIN")
+                .antMatchers("/").permitAll().and().formLogin();
+
     }
 }
